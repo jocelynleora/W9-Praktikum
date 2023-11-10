e560ee694a7739a7904d8b79e74c4ddf0146fbdb
jocelynleora <jocelynleora01@student.ciputra.ac.id>
2023-11-10 08:01:07 +0700

    Initial Commit

---
+++ W9-Praktikum/W9_PraktikumApp.swift
@@ -0,0 +1,17 @@
+//
+//  W9_PraktikumApp.swift
+//  W9-Praktikum
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct W9_PraktikumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
